9e48f2150729d0d8383b11848624071b45db2c15
jkjoseph2023@gmail.com <jkjoseph2023@gmail.com>
2024-08-13 21:15:24 +0530

    User Sign Up Done

---
+++ actions/auth-actions.js
@@ -0,0 +1,41 @@
+"use server";
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email || !email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (!password || password.length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length) {
+    return { errors };
+  }
+
+  //store it in db
+
+  const hashedPassword = hashUserPassword(password);
+
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if(error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      errors.email = "Email already in use";
+      return { errors };
+    }
+
+    throw error;
+  }
+
+  redirect("/training");
+}
--- components/auth-form.js
+++ components/auth-form.js
@@ -1,8 +1,17 @@
+'use client';
+
+
+import { signup } from '@/actions/auth-actions';
 import Link from 'next/link';
+import {useFormState} from 'react-dom';
 
 export default function AuthForm() {
+
+  const [formState, formAction] = useFormState(signup, {});
+
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -10,10 +19,12 @@ export default function AuthForm() {
         <label htmlFor="email">Email</label>
         <input type="email" name="email" id="email" />
       </p>
+      {formState.errors && formState.errors.email && <p id='form-errors'>{formState.errors.email}</p>}
       <p>
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && formState.errors.password && <p id='form-errors'>{formState.errors.password}</p>}
       <p>
         <button type="submit">
           Create Account
